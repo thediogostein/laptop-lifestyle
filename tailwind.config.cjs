936e2399bb8381d09dc90985ed2208b55bf11dd8
bruxo <diogo.stein@outlook.com.br>
2023-09-07 12:34:02 -0300

    Build hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,35 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '5rem',
+			}
+		},
+		screens: {
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
+
+			md: '768px',
+			// => @media (min-width: 768px) { ... }
+
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			// '2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		}
+
 	},
 	plugins: [],
 }
